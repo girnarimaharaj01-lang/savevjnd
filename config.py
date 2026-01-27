8e04f8d881965c75873bfa8e0ee983887a32fbaf
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-27 09:11:24 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import os
-
-# Login feature, if you want then True , if you don't want then False
-LOGIN_SYSTEM = bool(os.environ.get('LOGIN_SYSTEM', True)) # True or False
-
-if LOGIN_SYSTEM == False:
-    # if login system is False then fill your tg account session below 
-    STRING_SESSION = os.environ.get("STRING_SESSION", "8399557684")
-else:
-    STRING_SESSION = None
 
 # Bot token @Botfather
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "8031981205:AAGz3oAzc7hvRHViOirPutAJeiP3t4lvu40")
@@ -25,18 +12,8 @@ API_HASH = os.environ.get("API_HASH", "a04b3df726520026f207079aec2f9879")
 # Your Owner / Admin Id For Broadcast 
 ADMINS = int(os.environ.get("ADMINS", "8399557684"))
 
-# Your Channel Id In Which Bot Upload Downloaded Video/File/Message etc.
-# And Make Your Bot Admin In this channel with full rights.
-# if you don't want to upload in channel then leave it blank don't fill anything.
-CHANNEL_ID = os.environ.get("CHANNEL_ID", "8399557684")
-
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://girnarimaharaj01_db_user:KsxBY4eoUBwRKXXw@cluster0.6firafk.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_NAME = os.environ.get("DB_NAME", "girnarimaharaj01_db_user")
 
-# Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
-WAITING_TIME = int(os.environ.get("WAITING_TIME", "10")) # time in seconds
-
-# If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
-ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
