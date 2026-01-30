49094fab1ffdfd97dfeb765af793b8f38d8272bb
girnarimaharaj01-lang <girnarimaharaj01@gmail.com>
2026-01-29 21:03:11 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,9 @@
 # Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
+from config import API_ID, API_HASH, BOT_TOKEN, LOGIN_SYSTEM
 
-if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("TechVJ", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
-	TechVJUser.start()
+
 else:
     TechVJUser = None
 
